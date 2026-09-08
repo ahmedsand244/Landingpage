72853cfc504944d7524e51fad45a11c4df23fcf1
ahmedsand244 <ahmedsand244@gmail.com>
2026-09-08 10:24:43 +0300

    Turbocharge website speed: GZip compression, WhiteNoise caching, N+1 query prefetch, instant i18n

--- core_project/settings.py
+++ core_project/settings.py
@@ -33,7 +33,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.gzip.GZipMiddleware',  # High-speed response compression
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Instant static file serving & caching
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',  # i18n locale middleware
     'django.middleware.common.CommonMiddleware',
@@ -133,6 +135,13 @@ LOCALE_PATHS = [
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# High-Performance WhiteNoise Static Files Storage & Compression
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+WHITENOISE_MAX_AGE = 31536000  # 1 year cache for static assets
+
+# High-Performance Cached Database Sessions
+SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
 
 # Media files
 MEDIA_URL = '/media/'
